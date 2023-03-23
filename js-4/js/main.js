a9766e3719e7a34a9973c17eb7b79e342eed12e5
Tanja Schmidt <taschmi@gmx.net>
2023-03-23 07:32:03 +0100

    add rest of js-4 (numbers), plus js 5 (Math Methods)

--- js-4/js/main.js
+++ js-4/js/main.js
@@ -1,4 +1,24 @@
 // Numbers
 const myNumber = 42;
+const myFloat = 42.42;
+const myString = '42.123abc';
 
-console.log(myNumber);
+//Number Methods
+// The Numberi.isInteger() method determines whether the passed value is an integer.
+Number.isInteger(42); // true
+
+// The Number.parseFloat() method parses a string argument and returns a floating point number. If a number cannot be parsed from the  argument, t returns NaN (Not a Number).
+console.log(Number.parseFloat(myNumber)); // 42
+console.log(Number.parseFloat(myFloat)); // 42.42
+console.log(Number.parseFloat(myString)); // 42.123 (the first number in the string)
+
+// The Number.parseInt() method parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems). If a number cannot be parsed from the  argument, it returns NaN (Not a Number).
+console.log(Number.parseInt(myNumber)); // 42
+
+// The toFixed() method formats a number according to how many decimal points you provide as the parameter.
+console.log(myFloat.toFixed(2)); // '42.42' => this returns a string, not a number!
+console.log(Number(myFloat.toFixed(2))); // 42.42 => this returns a number  (Number() is a global function)
+
+i// The toString() method returns a string representing the specified Number object. The toString() method is used to convert a number to a string.
+console.log(myNumber.toString()); // '42'
+
---
+++ js-5/js/main.js
@@ -0,0 +1,42 @@
+// Math Methods and Properties
+
+// Pi
+console.log(Math.PI);
+
+// The Math.trunc() method returns the integer part of a number by removing any fractional digits.
+console.log(Math.trunc(Math.PI)); // returns 3
+// The Math.round() method returns the value of a number rounded to the nearest integer.
+console.log(Math.round(Math.PI)); // returns 3
+console.log(Math.round(4.7)); // returns 5
+console.log(Math.round(4.4)); // returns 4
+// The Math.ceil() method returns the smallest integer greater than or equal to a given number.
+console.log(Math.ceil(Math.PI)); // returns 4
+console.log(Math.ceil(4.7)); // returns 5
+console.log(Math.ceil(4.4)); // returns 5
+// The Math.floor() method returns the largest integer less than or equal to a given number.
+console.log(Math.floor(Math.PI)); // returns 3
+console.log(Math.floor(4.7)); // returns 4
+console.log(Math.floor(4.4)); // returns 4
+
+// The Math.pow() method returns the base to the exponent power, that is, baseexponent.
+console.log(Math.pow(2, 3)); // returns 8
+console.log(Math.pow(3, 2)); // returns 9
+// The Math.min() method returns the lowest-valued number passed into it, or NaN if any parameter isn't a number and
+// can't be converted into one.
+console.log(Math.min(2, 3)); // returns 2
+console.log(Math.min(3, 2)); // returns 2
+console.log(Math.min(2, 3, 4, 5)); // returns 2
+console.log(Math.min(2, 3, 4, 5, 1)); // returns 1
+// The Math.max() method returns the highest-valued number passed into it, or NaN if any parameter isn't a number and can't be converted into one.
+console.log(Math.max(2, 3)); // returns 3
+console.log(Math.max(3, 2)); // returns 3
+console.log(Math.max(2, 3, 4, 5)); // returns 5
+console.log(Math.max(2, 3, 4.34, 5.67, 1)); // returns 5.67
+console.log(Math.max(2, 3, 4, 5, 1, 'a')); // returns NaN
+console.log(Math.max(2, 3, 4, 5, 1, 'a', 6)); // returns NaN
+
+// The Math.random() method returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — which you can then scale to your desired range. The implementation selects the initial seed to the random number generation algorithm; it cannot be chosen or reset by the user.
+console.log(Math.random()); // returns a random number
+console.log(Math.floor(Math.random() * 10) + 1); // returns a random number from 1 to 10
+// Tip: Always choose Math.floor(), not Math.ceil() for generating a number from 1 to 1ß, because Math.ceil() has a chance or returning 0
+
